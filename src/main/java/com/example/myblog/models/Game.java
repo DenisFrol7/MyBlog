ab734dfab4385480777aba2cabd19b3a4123b15c
Denis <domino387@gmail.com>
2023-03-06 23:45:29 +0300

    began to develop a section with games

---
+++ src/main/java/com/example/myblog/models/Game.java
@@ -0,0 +1,37 @@
+package com.example.myblog.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String status;
+    private String genre;
+    private String platform;
+    private LocalDate release;
+    private LocalDate playDate;
+    private String library;
+    private Integer achievementsUnlock;
+    private Integer achievementsCount;
+    private Integer gameScore;
+    private Integer gameScoreCount;
+
+    public int progress() {
+        if (achievementsCount != null) {
+            double d = (double) achievementsUnlock / achievementsCount;
+            return (int) (d * 100);
+        } else return 0;
+    }
+}
---
+++ src/main/java/com/example/myblog/services/GameService.java
@@ -0,0 +1,4 @@
+package com.example.myblog.services;
+
+public class GameService {
+}
